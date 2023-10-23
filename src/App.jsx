33a2ad295d8381173cbf29cbe92062201ff1a190
lqasemzadeh <lqasemzadeh@gmail.com>
2023-10-23 10:05:33 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import NavbarComponent from "./components/ui/Navbar.component";
+import FooterComponent from "./components/ui/Footer.component";
+
+
+function App() {
+  return (
+    <div>
+      <NavbarComponent />
+      <div className="wrapper">
+          wrapper
+      </div>
+      <FooterComponent />
+
+    </div>
+  );
+}
+
+export default App;
