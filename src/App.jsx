4fa3730071a2ab1597859c31f547827d48f4fa5f
lqasemzadeh <lqasemzadeh@gmail.com>
2023-10-23 12:17:58 +0330

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import NavbarComponent from "./components/ui/Navbar.component";
 import FooterComponent from "./components/ui/Footer.component";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home.page";
+import ProductsPage from "./pages/Products.page";
+import NewsPage from "./pages/News.page";
+import AboutPage from "./pages/About.page";
+import ContactPage from "./pages/Contact.page";
 
 
 function App() {
@@ -7,7 +13,13 @@ function App() {
     <div>
       <NavbarComponent />
       <div className="wrapper">
-          wrapper
+          <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/products" element={<ProductsPage />} />
+              <Route path="/news" element={<NewsPage />} />
+              <Route path="/about-us" element={<AboutPage />} />
+              <Route path="/contact-us" element={<ContactPage />} />
+          </Routes>
       </div>
       <FooterComponent />
 
