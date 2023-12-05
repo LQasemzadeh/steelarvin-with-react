bd8d01790dcf2fc490ce8f533afe33775652c830
lqasemzadeh <lqasemzadeh@gmail.com>
2023-12-05 11:31:22 +0330

    NavBar

--- src/components/ui/Navbar.component.jsx
+++ src/components/ui/Navbar.component.jsx
@@ -1,52 +1,11 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-
 
 const NavbarComponent = () => {
     return (
-        <div className="container">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">استیل آروین تاو </Link>
-
-                    <div className="d-flex justify-content-center">
-                        <ul className="navbar-nav">
-
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">خانه</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/products">محصولات و خدمات</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/gallery">گالری تصاویر</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/news">اخبار</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/about-us">درباره ما</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/contact-us">تماس با ما</Link>
-                            </li>
-
-                        </ul>
-                    </div>
-
-
-                </div>
-
-                <form className="container-fluid justify-content-end">
-                    <button className="btn btn-sm btn-outline-secondary" type="button">مشاوره رایگان</button>
-                </form>
-
-            </nav>
+        <div>
+            
         </div>
-
     );
 };
 
 export default NavbarComponent;
-
