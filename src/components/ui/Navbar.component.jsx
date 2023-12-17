d6bf66a6bdb66c3396691f88a858e36b5e9c5783
lqasemzadeh <lqasemzadeh@gmail.com>
2023-12-17 14:09:09 +0330

    popup

--- src/components/ui/Navbar.component.jsx
+++ src/components/ui/Navbar.component.jsx
@@ -70,30 +70,27 @@ const NavbarComponent = () => {
                                     {/* if there is a button in form, it will close the modal */}
                                     <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                                 </form>
-                                <div className="absolute space-x-1 flex flex-row">
-                                    <input
-                                        type="text"
-                                        className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-orange-800 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                                        placeholder="جستجو کنید ..."
-                                    />
-                                    <button className="px-4 text-white bg-orange-800 rounded-full ">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            className="w-5 h-5"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                            strokeWidth={2}
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                                            />
-                                        </svg>
-                                    </button>
-
-                                </div>
+                                <input
+                                    type="text"
+                                    className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-orange-800 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                                    placeholder="جستجو کنید ..."
+                                />
+                                <button className="px-4 text-white bg-orange-800 rounded-full ">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-5 h-5"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                        />
+                                    </svg>
+                                </button>
                             </div>
                         </dialog>
                     </div>
